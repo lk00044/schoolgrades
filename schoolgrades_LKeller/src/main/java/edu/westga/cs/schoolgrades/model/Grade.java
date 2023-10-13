fd8da94ed38c93130b1928e6aec01564828905a6
Leslie Keller <lk00044@my.westga.edu>
2023-10-13 12:53:05 -0400

    Created interface Grade and class SimpleGrade

---
+++ schoolgrades_LKeller/src/main/java/edu/westga/cs/schoolgrades/model/Grade.java
@@ -0,0 +1,15 @@
+package edu.westga.cs.schoolgrades.model;
+
+/**
+ * The grade Interface provides the base for the concrete
+ * Grade classes
+ * 
+ * @author	Leslie Keller
+ * @version	13Oct2023
+ */
+
+public interface Grade {
+	
+	public double getValue();
+
+}
