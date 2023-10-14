be949fbf664114b48fe57deece505ebfa0aefbf9
Leslie Keller <lk00044@my.westga.edu>
2023-10-14 09:42:21 -0400

    SimpleGrade Test

---
+++ schoolgrades_LKeller/src/test/java/edu/westga/cs/schoolgrades/model/TestSimpleGrade.java
@@ -0,0 +1,30 @@
+package edu.westga.cs.schoolgrades.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestSimpleGrade {
+
+	@Test
+	void testSimpleGradeNegativeValue() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			new SimpleGrade(-4);
+		});
+	}
+	
+	@Test
+	void testSimpleGrade0Value() {
+		SimpleGrade grade = new SimpleGrade(0);
+		assertEquals(0, grade.getValue(), 0.0001);
+	}
+	
+	@Test
+	void testSimpleGrade100Value() {
+		SimpleGrade grade = new SimpleGrade(100);
+		assertEquals(100, grade.getValue(), 0.0001);
+	}
+
+}
