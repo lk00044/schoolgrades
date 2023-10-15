7abb74ebe81670332f885c67370a28aee66ad8e0
Leslie Keller <lk00044@my.westga.edu>
2023-10-15 09:10:02 -0400

    Update to WeightedGrade

--- schoolgrades_LKeller/src/main/java/edu/westga/cs/schoolgrades/model/WeightedGrade.java
+++ schoolgrades_LKeller/src/main/java/edu/westga/cs/schoolgrades/model/WeightedGrade.java
@@ -1,44 +1,31 @@
 package edu.westga.cs.schoolgrades.model;
 
+
 /**
- * The WeightedGrade represents a weighted grade grade
+ * Decorator for grade to apply weight
  * 
  * @author 	Leslie Keller
- * @version	13Oct2023
+ * @version	14Oct2023
  */
 public class WeightedGrade implements Grade{
-	
-	private double value;
+	protected Grade grade;
 	private double weight;
 	
-	/**
-	 * Constructor to create the WeightedGrade object
-	 * @param: 			value	The value of the grade
-	 * Precondition: 	value cannot be negative. 
-	 */
-	public WeightedGrade(double value, double weight) {
-		if (value < 0)
-		{
-			throw new IllegalArgumentException("Grade cannot be negative.");
+	public WeightedGrade(Grade grade, double weight) {
+		if (grade.getValue() < 0) {
+			throw new IllegalArgumentException("Negative grades not allowed.");
+			
 		}
-		if (weight < 0)
-		{
-			throw new IllegalArgumentException("Grade weight cannot be negative.");
+		if (weight < 0) {
+			throw new IllegalArgumentException("Negative weights not allowed.");
+			
 		}
-		this.value = value;
+		this.grade = grade;
 		this.weight = weight;
 	}
-
-	/**
-	 * This method returns the value of the simplegrade object.
-	 * @return	value	the value of the grade
-	 */
-	@Override
-	public double getValue() {
-		return this.value * this.weight;
-	//	Grade wtdGrade = new WeightDecorator(this);
-	//	return wtdGrade.getValue();
-		
+	
+	public double getValue() {		
+		return this.grade.getValue() * this.weight;
 	}
 
 }
--- schoolgrades_LKeller/src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGrade.java
+++ schoolgrades_LKeller/src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGrade.java
@@ -6,42 +6,41 @@ import org.junit.jupiter.api.Test;
 
 class TestWeightedGrade {
 
-	@Test
-	void testWeightedGradeNegativeValue() {
-		assertThrows(IllegalArgumentException.class, () -> {
-			new WeightedGrade(-4, 100);
-		});
-	}
 	
 	@Test
 	void testWeightedGradeNegativeWeight() {
+		Grade grade = new SimpleGrade(94);
 		assertThrows(IllegalArgumentException.class, () -> {
-			new WeightedGrade(94, -100);
+			new WeightedGrade(grade, -100);
 		});
 	}
 	
 	@Test
 	void testWeightedGradeIntGradeDecimalWeight() {
-		WeightedGrade grade = new WeightedGrade(100, .85);
-		assertEquals(85, grade.getValue(), .0001);
+		Grade grade = new SimpleGrade(100);
+		Grade wtdGrade = new WeightedGrade(grade, .85);
+		assertEquals(85, wtdGrade.getValue(), .0001);
 	}
 	
 	@Test
 	void testWeightedGradeDecimalGradeIntWeight() {
-		WeightedGrade grade = new WeightedGrade(79.0, 1);
-		assertEquals(79.0, grade.getValue(), .0001);
+		Grade grade = new SimpleGrade(79.0);
+		Grade wtdGrade = new WeightedGrade(grade, 1);
+		assertEquals(79.0, wtdGrade.getValue(), .0001);
 	}
 	
 	@Test
 	void testWeightedGradeDecimalGradeDecimalWeight() {
-		WeightedGrade grade = new WeightedGrade(200.0, 0.50);
-		assertEquals(100.0, grade.getValue(), .0001);
+		Grade grade = new SimpleGrade(200);
+		Grade wtdGrade = new WeightedGrade(grade, 0.50);
+		assertEquals(100.0, wtdGrade.getValue(), .0001);
 	}
 	
 	@Test
 	void testWeightedGradeIntGradeIntWeight() {
-		WeightedGrade grade = new WeightedGrade(98, 1);
-		assertEquals(98.0, grade.getValue(), .0001);
+		Grade grade = new SimpleGrade(98);
+		Grade wtdGrade = new WeightedGrade(grade, 1);
+		assertEquals(98.0, wtdGrade.getValue(), .0001);
 	}
 
 }
