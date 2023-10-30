6db743b9cc83dfd78a0992e3154a609e972933e7
Leslie Keller <lk00044@my.westga.edu>
2023-10-30 14:15:18 -0400

    work on controller

--- src/main/java/edu/westga/cs/schoolsgrades/controller/SchoolGradesController.java
+++ src/main/java/edu/westga/cs/schoolsgrades/controller/SchoolGradesController.java
@@ -6,18 +6,31 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.util.converter.NumberStringConverter;
 
 public class SchoolGradesController {
 
 	@FXML private TextField txtSubTotQuiz;
 	@FXML private TextField txtSubTotHW;
 	@FXML private TextField txtSubTotExam;
+	@FXML private TextField txtFinalGrade;
     @FXML private ListView <Grade> lstQuizGrades;
     @FXML private ListView <Grade> lstHWGrades;
     @FXML private ListView <Grade> lstExamGrades;
+    
     public static final ObservableList<Grade> quizGrades = FXCollections.observableArrayList();    
     public static final ObservableList<Grade> hwGrades = FXCollections.observableArrayList();    
     public static final ObservableList<Grade> examGrades =  FXCollections.observableArrayList();
     
-    
+    /**
+     * initialize the game
+     * @precondition: none
+     * @postcondition: 	School Grades is initialized
+     */
+    @FXML	
+    private void initialize() {
+    	
+
+    	// this.txtScore.textProperty().bindBidirectional(this.score, new NumberStringConverter());  	
+    }
 }
