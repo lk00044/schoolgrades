ba9b878ee74ce9f64aec6ac4600d8f57b0c58e79
Leslie Keller <lk00044@my.westga.edu>
2023-11-15 11:56:52 -0500

    Updated for demo in instructions

--- src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeConstructor.java
+++ src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeConstructor.java
@@ -2,8 +2,8 @@ package edu.westga.cs.schoolgrades.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
 
 public class TestWeightedGradeConstructor {
 
